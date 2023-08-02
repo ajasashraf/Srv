c1ec22b9c8465d9ce5801162fa269044ed29e794
Ajas Ashraf <ajasashraf7010@gmail.com>
2023-08-02 17:46:56 +0530

    Status listing with API completed.

--- client/src/components/CheckBox/CheckBox.jsx
+++ client/src/components/CheckBox/CheckBox.jsx
@@ -1,137 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 export default function CheckBox() {
+  const [status, setStatus] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://crmapi.srvinfotech.com/leads/leadsStatus")
+      .then((response) => {
+        setStatus(response.data.data);
+      })
+      .catch((error) => {
+        console.log("Error fetching user details:", error);
+      });
+  });
+
   return (
     <div>
       <ul className="items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex px-2 py-2">
-        <li className=" dark:border-gray-600">
-          <div className="flex items-center pl-3">
-            <input
-              id="Status"
-              type="checkbox"
-              value=""
-              className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded"
-            />
-            <label
-              for="laravel-checkbox-list"
-              className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-black"
-            >
-              Status
-            </label>
-          </div>
-        </li>
-        <li className=" dark:border-gray-600">
-          <div className="flex items-center pl-3">
-            <input
-              id="Status"
-              type="checkbox"
-              value=""
-              className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded"
-            />
-            <label
-              for="laravel-checkbox-list"
-              className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-black"
-            >
-              Status
-            </label>
-          </div>
-        </li>
-        <li className=" dark:border-gray-600">
-          <div className="flex items-center pl-3">
-            <input
-              id="Status"
-              type="checkbox"
-              value=""
-              className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded"
-            />
-            <label
-              for="laravel-checkbox-list"
-              className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-black"
-            >
-              Status
-            </label>
-          </div>
-        </li>
-        <li className=" dark:border-gray-600">
-          <div className="flex items-center pl-3">
-            <input
-              id="Status"
-              type="checkbox"
-              value=""
-              className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded"
-            />
-            <label
-              for="laravel-checkbox-list"
-              className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-black"
-            >
-              Status
-            </label>
-          </div>
-        </li>
-        <li className=" dark:border-gray-600">
-          <div className="flex items-center pl-3">
-            <input
-              id="Status"
-              type="checkbox"
-              value=""
-              className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded"
-            />
-            <label
-              for="laravel-checkbox-list"
-              className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-black"
-            >
-              Status
-            </label>
-          </div>
-        </li>
-        <li className=" dark:border-gray-600">
-          <div className="flex items-center pl-3 ">
-            <input
-              id="Status"
-              type="checkbox"
-              value=""
-              className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded"
-            />
-            <label
-              for="laravel-checkbox-list"
-              className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-black"
-            >
-              Status
-            </label>
-          </div>
-        </li>
-        <li className=" dark:border-gray-600">
-          <div className="flex items-center pl-3">
-            <input
-              id="Status"
-              type="checkbox"
-              value=""
-              className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded"
-            />
-            <label
-              for="laravel-checkbox-list"
-              className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-black"
-            >
-              Status
-            </label>
-          </div>
-        </li>
-        <li className=" dark:border-gray-600">
-          <div className="flex items-center pl-3">
-            <input
-              id="Status"
-              type="checkbox"
-              value=""
-              className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 "
-            />
-            <label
-              for="laravel-checkbox-list"
-              className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-black"
-            >
-              Status
-            </label>
-          </div>
-        </li>
+        {status.map((statusList) => (
+          <li className=" dark:border-gray-600">
+            <div className="flex items-center pl-3" key={statusList.id}>
+              <input
+                id="Status"
+                type="checkbox"
+                value=""
+                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded"
+              />
+              <label
+                htmlFor="Status-list"
+                className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-black whitespace-nowrap"
+              >
+                {statusList.name}
+              </label>
+            </div>
+          </li>
+        ))}
         <div className="flex w-full justify-end ">
           <input
             type="date"
